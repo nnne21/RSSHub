018929a63a0ed696148fd841e441602a76420e1f
nnne21 <68050383+nnne21@users.noreply.github.com>
2025-08-08 16:42:48 +0900

    Update vercel.ts

--- api/vercel.ts
+++ api/vercel.ts
@@ -1,15 +1,15 @@
 const path = require('node:path');
 const moduleAlias = require('module-alias');
-moduleAlias.addAlias('@', path.join(__dirname, '../dist'));
+moduleAlias.addAlias('@', path.join(__dirname, './dist'));
 
-const { setConfig } = require('../dist/config');
+const { setConfig } = require('./dist/config');
 setConfig({
     NO_LOGFILES: true,
 });
 
 const { handle } = require('hono/vercel');
-const app = require('../dist/app');
-const logger = require('../dist/utils/logger');
+const app = require('./dist/app');
+const logger = require('./dist/utils/logger');
 
 logger.info(`🎉 RSSHub is running! Cheers!`);
 

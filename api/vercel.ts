905319ed4649c9cc302eb15d3218ef3997fa0870
nnne21 <68050383+nnne21@users.noreply.github.com>
2025-08-08 21:34:36 +0900

    Create vercel.ts

---
+++ api/vercel.ts
@@ -0,0 +1,11 @@
+// api/vercel.ts（ESM, TypeScriptを直接import）
+import { handle } from 'hono/vercel';
+
+import { setConfig } from '../lib/config';
+import app from '../lib/app';
+import logger from '../lib/utils/logger';
+
+setConfig({ NO_LOGFILES: true });
+logger.info('🎉 RSSHub is running! Cheers!');
+
+export default handle(app);

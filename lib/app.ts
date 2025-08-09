b22437ded3f1c537f262cf8a92bc23ed603e614a
nnne21 <68050383+nnne21@users.noreply.github.com>
2025-08-10 00:51:35 +0900

    Update app.ts

--- lib/app.ts
+++ lib/app.ts
@@ -1,5 +1,5 @@
 // This file ensures that the request rewriter runs before the app
 
-import './utils/request-rewriter.js';
+// import './utils/request-rewriter.js';
 
 export default (await import('./app-bootstrap.js')).default;
